7eb43ed7ba1c5be3a424feca1b059cefe5a5ae2a
Bruno Moutinho <brunoe.moutinho@gmail.com>
2021-04-26 22:51:41 -0300

    Add initial component and tests that validate the form elements are present

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label htmlFor="question">Question</label>
+        <input id="question" placeholder="Can I have another donut?"></input>
+
+        <label htmlFor="askee">Askee</label>
+        <input id="askee" placeholder="Anonymous"></input>
+
+        <label htmlFor="status">Status</label>
+        <select id="status">
+          <option value="Unanswered">Unanswered</option>
+          <option value="Accepted">Accepted</option>
+          <option value="Rejected">Rejected</option>
+        </select>
+
+        <button>Save</button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, within } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("When the app loads, there should be an input to enter a question", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const inputElement = screen.getByLabelText(/question/i);
+  expect(inputElement).toBeInTheDocument();
+});
+
+test("When the app loads, there should be an input to enter the askee", () => {
+  render(<App />);
+  const inputElement = screen.getByLabelText(/askee/i);
+  expect(inputElement).toBeInTheDocument();
+});
+
+test("When the app loads, there should be an input to select the status of the question", () => {
+  render(<App />);
+  const selectElement = screen.getByLabelText(/status/i);
+  expect(selectElement).toBeInTheDocument();
+});
+
+test("When the app loads, there should be valid options for the status of the question", () => {
+  render(<App />);
+  const selectElement = screen.getByLabelText(/status/i);
+  const unansweredElement = within(selectElement).getByText(/unanswered/i);
+  expect(unansweredElement).toBeInTheDocument();
+
+  const acceptedElement = within(selectElement).getByText(/accepted/i);
+  expect(acceptedElement).toBeInTheDocument();
+
+  const rejectedElement = within(selectElement).getByText(/rejected/i);
+  expect(rejectedElement).toBeInTheDocument();
+});
+
+test("When the app loads, there should be a button to save the question", () => {
+  render(<App />);
+  const buttonElement = screen.getByText(/save/i);
+  expect(buttonElement).toBeInTheDocument();
 });
