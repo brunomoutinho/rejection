3aa5c4847fd0c01be1c33f6394a846b509f3193b
Bruno Moutinho <brunoe.moutinho@gmail.com>
2021-07-25 21:22:01 -0300

    Create a fetch controller with redux-dsm and add tests

--- src/store/controllers/index.js
+++ src/store/controllers/index.js
@@ -1,20 +1,26 @@
-export const requestSent = () => ({
-  type: requestSent.type
-})
-requestSent.type = 'REQUEST_SENT'
+import dsm from 'redux-dsm';
 
-export const STATUS_IDLE = 'idle'
-export const STATUS_LOADING = 'loading'
-const initialState = {
-  networkStatus: STATUS_IDLE
-}
-export const reducer = (state = initialState, action = {}) => {
-  switch (action.type) {
-    case requestSent.type:
-      return { ...state, networkStatus: STATUS_LOADING }
-    default:
-      return state
-  }
-}
+export const STATUS_IDLE = 'idle';
+export const STATUS_FETCHING = 'fetching';
+export const STATUS_ERROR = 'error';
+export const STATUS_SUCCESS = 'success';
 
-export const getNetworkStatus = state => state.networkStatus
+const fetchingStates = [
+  'initial',
+  STATUS_IDLE,
+  [
+    'fetch',
+    STATUS_FETCHING,
+    ['cancel', STATUS_IDLE],
+    ['report error', STATUS_ERROR, ['handle error', STATUS_IDLE]],
+    ['report success', STATUS_SUCCESS, ['handle success', STATUS_IDLE]],
+  ],
+];
+
+export const fetchController = dsm({
+  component: 'fetchController',
+  description: 'controls the state of the controllers for fetch',
+  actionStates: fetchingStates,
+});
+
+export const getStatus = (state) => state.status;
--- src/store/controllers/index.test.js
+++ src/store/controllers/index.test.js
@@ -1,20 +1,103 @@
-import { requestSent, reducer, getNetworkStatus, STATUS_IDLE, STATUS_LOADING } from "./index";
+import {
+  fetchController,
+  getStatus,
+  STATUS_IDLE,
+  STATUS_FETCHING,
+  STATUS_ERROR,
+  STATUS_SUCCESS,
+} from './index';
 
-const generateStore = ({ networkStatus = STATUS_IDLE } = {}) => ({ networkStatus })
+const generateStore = ({ status = STATUS_IDLE } = {}) => ({
+  status,
+  payload: { type: 'empty' },
+});
 
 describe('Controllers store', () => {
   test('Default store state', () => {
-    const expected = generateStore()
-    const actual = reducer()
+    const actual = fetchController.reducer();
+    const expected = generateStore();
 
-    expect(actual).toStrictEqual(expected)
-  })
+    expect(actual).toStrictEqual(expected);
+  });
 
   test('When I send a request, network status updates to loading', () => {
-    const actions = [requestSent()]
-    const expected = STATUS_LOADING
-    const actual = getNetworkStatus(actions.reduce(reducer, undefined))
+    const action = fetchController.actionCreators.fetch();
 
-    expect(actual).toBe(expected)
-  })
-})
+    const actual = getStatus(fetchController.reducer(undefined, action));
+    const expected = STATUS_FETCHING;
+
+    expect(actual).toBe(expected);
+  });
+
+  test("When I'm in fetching state and send a cancel action, status updates to idle", () => {
+    const actions = [
+      fetchController.actionCreators.fetch(),
+      fetchController.actionCreators.cancel(),
+    ];
+
+    const actual = getStatus(
+      actions.reduce(fetchController.reducer, undefined)
+    );
+    const expected = STATUS_IDLE;
+
+    expect(actual).toBe(expected);
+  });
+
+  test("When I'm in fetching state and send a report error action, status updates to error", () => {
+    const actions = [
+      fetchController.actionCreators.fetch(),
+      fetchController.actionCreators.reportError(),
+    ];
+
+    const actual = getStatus(
+      actions.reduce(fetchController.reducer, undefined)
+    );
+    const expected = STATUS_ERROR;
+
+    expect(actual).toBe(expected);
+  });
+
+  test("When I'm in error state and send a handle error action, status updates to idle", () => {
+    const actions = [
+      fetchController.actionCreators.fetch(),
+      fetchController.actionCreators.reportError(),
+      fetchController.actionCreators.handleError(),
+    ];
+
+    const actual = getStatus(
+      actions.reduce(fetchController.reducer, undefined)
+    );
+    const expected = STATUS_IDLE;
+
+    expect(actual).toBe(expected);
+  });
+
+  test("When I'm in fetching state and send a report success action, status updates to success", () => {
+    const actions = [
+      fetchController.actionCreators.fetch(),
+      fetchController.actionCreators.reportSuccess(),
+    ];
+
+    const actual = getStatus(
+      actions.reduce(fetchController.reducer, undefined)
+    );
+    const expected = STATUS_SUCCESS;
+
+    expect(actual).toBe(expected);
+  });
+
+  test("When I'm in success state and send a handle success action, status updates to idle", () => {
+    const actions = [
+      fetchController.actionCreators.fetch(),
+      fetchController.actionCreators.reportSuccess(),
+      fetchController.actionCreators.handleSuccess(),
+    ];
+
+    const actual = getStatus(
+      actions.reduce(fetchController.reducer, undefined)
+    );
+    const expected = STATUS_IDLE;
+
+    expect(actual).toBe(expected);
+  });
+});
