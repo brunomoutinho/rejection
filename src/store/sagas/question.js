4a624d7df5915f54ad829e0a5a5cd14fbd831e1b
Bruno Moutinho <brunoe.moutinho@gmail.com>
2021-07-25 21:23:35 -0300

    Update create question saga tests

--- src/store/sagas/question.js
+++ src/store/sagas/question.js
@@ -1,9 +1,22 @@
-import { call, put } from 'redux-saga/effects'
-import { saveQuestion } from '../../api'
-import { requestSent } from '../controllers'
+import { call, put } from 'redux-saga/effects';
+import { saveQuestion } from '../../api';
+import { fetchController } from '../controllers';
+import { createQuestion } from '../question';
 
 export function* createQuestionSaga(question) {
-  yield put(requestSent())
-  yield call(saveQuestion, question)
-  /** yield put(createQuestion, { payload: data }) */
+  yield put(fetchController.actionCreators.fetch());
+  yield put(createQuestion(question));
+  try {
+    const result = yield call(saveQuestion, question);
+
+    if (result.status === 'success') {
+      yield put(fetchController.actionCreators.reportSuccess());
+      yield put(fetchController.actionCreators.handleSuccess(result.payload));
+    } else if (result.status === 'error') {
+      yield put(fetchController.actionCreators.reportError());
+      yield put(fetchController.actionCreators.handleError(result.payload));
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
--- src/store/sagas/question.test.js
+++ src/store/sagas/question.test.js
@@ -1,26 +1,101 @@
-import { call, put } from 'redux-saga/effects'
-import { requestSent } from '../controllers'
-import { createQuestion } from '../question'
-import { createQuestionSaga } from './question'
-import { saveQuestion } from '../../api'
+import { call, put } from 'redux-saga/effects';
+import { fetchController } from '../controllers';
+import { createQuestion } from '../question';
+import { createQuestionSaga } from './question';
+import { saveQuestion } from '../../api';
+// import { fetchController } from '../controllers';
 
 describe('Question sagas', () => {
-  test('Running the create question saga results in a call to save the question in the DB', () => {
-    const question = createQuestion().payload
-    const createQuestionGen = createQuestionSaga(question)
+  test('Running the create question saga saves the question in the DB and the store', () => {
+    const question = createQuestion().payload;
+    const createQuestionGen = createQuestionSaga(question);
 
     {
-      const expected = put(requestSent())
-      const actual = createQuestionGen.next().value
+      const expected = put(fetchController.actionCreators.fetch());
+      const actual = createQuestionGen.next().value;
 
-      expect(actual).toStrictEqual(expected)
+      expect(actual).toStrictEqual(expected);
     }
 
     {
-      const expected = call(saveQuestion, { data: question })
-      const actual = createQuestionGen.next().value
+      const expected = put(createQuestion(question));
+      const actual = createQuestionGen.next().value;
 
-      expect(actual).toStrictEqual(expected)
+      expect(actual).toStrictEqual(expected);
     }
-  })
-})
+
+    {
+      const expected = call(saveQuestion, question);
+      const actual = createQuestionGen.next().value;
+
+      expect(actual).toStrictEqual(expected);
+    }
+
+    {
+      const expected = put(fetchController.actionCreators.reportSuccess());
+      const actual = createQuestionGen.next({
+        status: 'success',
+        payload: question,
+      }).value;
+
+      expect(actual).toStrictEqual(expected);
+    }
+
+    {
+      const expected = put(
+        fetchController.actionCreators.handleSuccess(question)
+      );
+      const actual = createQuestionGen.next().value;
+
+      expect(actual).toStrictEqual(expected);
+    }
+  });
+
+  test('Running the create question saga, rolls back the change in store if an error occurs', () => {
+    const question = createQuestion().payload;
+    const createQuestionGen = createQuestionSaga(question);
+
+    {
+      const expected = put(fetchController.actionCreators.fetch());
+      const actual = createQuestionGen.next().value;
+
+      expect(actual).toStrictEqual(expected);
+    }
+
+    {
+      const expected = put(createQuestion(question));
+      const actual = createQuestionGen.next().value;
+
+      expect(actual).toStrictEqual(expected);
+    }
+
+    {
+      const expected = call(saveQuestion, question);
+      const actual = createQuestionGen.next().value;
+
+      expect(actual).toStrictEqual(expected);
+    }
+
+    {
+      const expected = put(fetchController.actionCreators.reportError());
+      const actual = createQuestionGen.next({
+        status: 'error',
+        payload: { question, message: 'Error' },
+      }).value;
+
+      expect(actual).toStrictEqual(expected);
+    }
+
+    {
+      const expected = put(
+        fetchController.actionCreators.handleError({
+          question,
+          message: 'Error',
+        })
+      );
+      const actual = createQuestionGen.next().value;
+
+      expect(actual).toStrictEqual(expected);
+    }
+  });
+});
